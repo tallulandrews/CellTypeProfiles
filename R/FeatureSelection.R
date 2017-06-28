a6a2063eae464028e96284f068cb2357e63c4e98
Tallulah Andrews <ta6@sanger.ac.uk>
2017-06-28 11:00:09 +0100

    Add HVG, DANB, Marker feature selection & start documentation for them

--- R/FeatureSelection.R
+++ R/FeatureSelection.R
@@ -15,7 +15,25 @@ m3drop.features <- function(expr_mat, clusters) {
         return(as.numeric(rownames(expr_mat) %in% f_names))
 } 
 
-marker.features <- function(expr_mat, clusters) {
-        # To Do
+danb.features <- function(expr_mat, clusters) {
+        require("M3Drop")
+	counts <- NBumiConvertToInteger(expr_mat);
+	fit <- NBumiFitModel(counts);
+	topX <- ceiling(0.05*length(counts[,1]))
+	f_names <- names(NBumiFeatureSelectionCombinedDrop(fit)[1:topX])
+        return(as.numeric(rownames(expr_mat) %in% f_names))
 }
 
+hvg.features <- function (expr_mat, clusters) {
+        require("M3Drop")
+        features <- BrenneckeGetVariableGenes(expr_mat, suppress.plot=TRUE, fdr=0.05);
+        f_names <- rownames(features[features$q.value < 0.05,])
+        return(as.numeric(rownames(expr_mat) %in% f_names))
+}
+
+marker.features <- function(expr_mat, clusters) {
+	markers = complex_markers(expr_mat, clusters, n_max=length(unique(clusters))-1, strict_only=FALSE)
+	auc_filter = markers[,1] > 0.7;
+	sig_filter = markers$q.value < 0.05;
+	return(as.numeric(auc_filter & sig_filter));
+}
